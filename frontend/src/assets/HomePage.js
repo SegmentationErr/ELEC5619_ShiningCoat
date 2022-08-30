b301f6e45c311250013a9ec6f8d7806ac3d88a87
Dehuan Zheng <dzhe4305@uni.sydney.edu.au>
2022-08-30 22:37:11 +1000

    create draft home page static UI

--- frontend/src/assets/HomePage.js
+++ frontend/src/assets/HomePage.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Row, Col, Input, Menu } from 'antd';
+import { Row, Col, Image } from 'antd';
 import axios from 'axios';
-import NavBar from './NavBar';
 import showAlert from './Alert';
+
+
+import '../css/homePage.css'
+import { withRouter } from './withRouter';
+
 
 class HomePage extends Component {
     constructor(props) {
@@ -10,18 +14,83 @@ class HomePage extends Component {
     }
 
     state = {
-        isModalVisible: false
+        isModalVisible: false,
+        data: [
+            {
+                name: "service1",
+                location: "service1 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            },
+            {
+                name: "service2",
+                location: "service2 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            },
+            {
+                name: "service3",
+                location: "service3 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            },
+            {
+                name: "service4",
+                location: "service4 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            },
+            {
+                name: "service5",
+                location: "service1 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            },
+            {
+                name: "service6",
+                location: "service2 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            },
+            {
+                name: "service7",
+                location: "service3 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            },
+            {
+                name: "service8",
+                location: "service4 location",
+                time: "9-17pm",
+                imgSrc: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+            }
+        ],
     }
 
     render() {
         return (
-            <div>
+            <div id="homePageMainDiv">
                 <Row>
-
+                    {this.state.data.map((service, key) => {
+                        return (
+                            <Col span={6}>
+                                <div class="homePageRecommendationInfoDiv">
+                                    <Image
+                                        style={{ width: 200, height: 200, borderRadius: 400 / 2 }}
+                                        width={200}
+                                        src={service.imgSrc}
+                                    />
+                                    <p>{service.name}</p>
+                                    <p>{service.location}</p>
+                                    <p>{service.time}</p>
+                                </div>
+                            </Col>
+                        )
+                    })}
                 </Row>
             </div>
         );
     }
 }
 
-export default HomePage;
+export default withRouter(HomePage);
