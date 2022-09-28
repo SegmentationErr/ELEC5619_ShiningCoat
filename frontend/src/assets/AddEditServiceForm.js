c5d608b8a1fb65629849ea2328ddbd5ea7114468
charles <yjc1320802502@gmail.com>
2022-09-28 18:10:13 +1000

    add edit service page

---
+++ frontend/src/assets/AddEditServiceForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AddEditServiceForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            
+        );
+    }
+}
+ 
+export default AddEditServiceForm;
